4c1474bde0f1b97aaab55f8d05707fb98be2a52f
LenkaIaco <117647840+LenkaIaco@users.noreply.github.com>
2022-11-23 17:54:18 +0100

    adjusting label name

---
+++ finalProjectW3/finalProject.js
@@ -0,0 +1,99 @@
+let imagebarBOAT = {
+    photo: 'images/boat.jpg',
+    imagetitle: "Water rippling",
+    imagedescription: "Motor boat waves gradually stretch out into a rainbow spectrum",
+}
+$('#boat').attr('src', imagebarBOAT.photo);
+
+let imagebarCAT = {
+    photo: 'images/cat.jpg',
+    imagetitle: "Cat rolling",
+    imagedescription: "Each hair plays into a fur-symphony of colors",
+}
+$('#cat').attr('src', imagebarCAT.photo);
+
+let imagebarCLOVERS = {
+    photo: 'images/clovers.jpg',
+    imagetitle: "Clover bed",
+    imagedescription: "Lush and colorful mosaic of nature",
+}
+$('#clovers').attr('src', imagebarCLOVERS.photo);
+
+let imagebarDISCOBALLS = {
+    photo: 'images/discoballs.jpg',
+    imagetitle: "Party time",
+    imagedescription: "Sparkling with every shade of fun",
+}
+$('#discoballs').attr('src', imagebarDISCOBALLS.photo);
+
+let imagebarDOG = {
+    photo: 'images/dog.jpg',
+    imagetitle: "Friend",
+    imagedescription: "Companion through both colorful and hard times",
+}
+$('#dog').attr('src', imagebarDOG.photo);
+
+let imagebarFACEPAINT = {
+    photo: 'images/facepaint.jpg',
+    imagetitle: "Mankind",
+    imagedescription: "In diversity united",
+}
+$('#facepaint').attr('src', imagebarFACEPAINT.photo);
+
+let imagebarHOTELFLOORS = {
+    photo: 'images/hotelfloors.jpg',
+    imagetitle: "Complexity",
+    imagedescription: "The deeper we look, the more we discover",
+}
+$('#hotelfloors').attr('src', imagebarHOTELFLOORS.photo);
+
+
+let imagebarNEBULA = {
+    photo: 'images/nebula.jpg',
+    imagetitle: "Universe",
+    imagedescription: "Infinite ways of manifestation",
+}
+$('#nebula').attr('src', imagebarNEBULA.photo);
+
+
+let imagebarPAINTPATTERN = {
+    photo: 'images/paintpattern.jpg',
+    imagetitle: "Art",
+    imagedescription: "A free form of expression",
+}
+$('#paintpattern').attr('src', imagebarPAINTPATTERN.photo);
+
+let imagebarRAINBOWINGALLERY = {
+    photo: 'images/rainbowingallery.jpg',
+    imagetitle: "Rainbow in gallery",
+    imagedescription: "Mankind's unique ability to manifest abstract thought into reality",
+}
+$('#rainbowingallery').attr('src', imagebarRAINBOWINGALLERY.photo);
+
+let imagebarSOAPBUBBLE = {
+    photo: 'images/soapbubble.jpg',
+    imagetitle: "SOAPBUBBLE",
+    imagedescription: "Fragility can create beauty",
+}
+$('#soapbubble').attr('src', imagebarSOAPBUBBLE.photo);
+
+let imagebarSPIRALSTAIRCASE = {
+    photo: 'images/spiralstaircase.jpg',
+    imagetitle: "STAIRCASE",
+    imagedescription: "Recurring patterns with changing colors",
+}
+$('#spiralstaircase').attr('src', imagebarSPIRALSTAIRCASE.photo);
+
+let Array = [imagebarBOAT, imagebarCAT, imagebarCLOVERS, imagebarDISCOBALLS, imagebarDOG, imagebarFACEPAINT, imagebarHOTELFLOORS, imagebarNEBULA, imagebarPAINTPATTERN, imagebarRAINBOWINGALLERY, imagebarSOAPBUBBLE, imagebarSPIRALSTAIRCASE, imagebarSUNBEDSBEACH, imagebarTORIGATES, imagebarTRIANGLES, imagebarTWISTINGMOUNTAINROAD ]
+
+
+
+// let myvar = $('.thumbnailimage').attr('src')
+// console.log(myvar)
+// console.log($('.centralimage').attr('src'))
+
+// $('.thumbnailimage').on('click', () => {
+//    console.log(event.target);
+// let myvar = $('.thumbnailimage').attr('src')
+
+//  })
